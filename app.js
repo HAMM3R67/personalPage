a3bda7f0e729c086a509353400149151929e7beb
HAMM3R67 <tcschauer@gmail.com>
2015-08-15 10:23:58 -0600

    trying to figure out how to get the route to work, I broke the {{name}}

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = angular.module('hammer', ['ngRoute']);
+
+// app.config(function($routeProvider, $httpProvider){
+// 	$httpProvider.interceptors.push('httpRequestInterceptor');
+// 	
+// 	$routeProvider
+// 	.when('/', {
+// 		templateUrl: 'index.html',
+// 		controller: 'js/controller.js'
+// 	})
+// 	.when('/Resume', {
+// 		templateUrl: 'Resume/resume.html',
+// 		controller: 'resumeController'
+// 	})
+// 	.otherwise({
+// 		redirectTo: '/'
+// 	})
+//});
