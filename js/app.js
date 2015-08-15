1738ddbc41d6db1373eb151623ab0cf02d4537af
HAMM3R67 <tcschauer@gmail.com>
2015-08-15 09:59:39 -0600

    setup a route, still need to make a link

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('hammer', []);
+var app = angular.module('hammer', [ngRoute]);
+
+app.config(function($routeProvider, $httpProvider){
+	$httpProvider.interceptors.push('httpRequestInterceptor');
+	
+	$routeProvider
+	.when('/', {
+		templateUrl: 'index.html',
+		controller: 'js/controller'
+	})
+	.when('/resume', {
+		templateUrl: 'Resume/resume.html',
+		controller: 'resumeController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	})
+});
