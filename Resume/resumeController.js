29f2d7190259727eb31830dda2aad12baabc02f0
HAMM3R67 <tcschauer@gmail.com>
2015-08-15 09:52:23 -0600

    added myDirective.js removed myDirective

---
+++ Resume/resumeController.js
@@ -0,0 +1,5 @@
+var app = angular.module('hammer')
+
+.controller('resumeCtrl', function($scope){
+	
+})
---
+++ Resume/resumeService.js
@@ -0,0 +1,5 @@
+var app = angular.module('hammer')
+
+.service('resumeService', function($scope){
+	
+})
